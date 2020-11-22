c778bb0f5d0ec90362f8903388f236b7f49225b7
David Snider <david@davidsnider.org>
2020-11-22 13:00:43 -0700

    fix uvicorn logging

--- src/cava/webhook/main.py
+++ src/cava/webhook/main.py
@@ -52,4 +52,4 @@ async def motion(motion: amcrest_motion):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="warning")
