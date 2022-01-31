d63abb972919a86acac96a9b2d0b9ae122a8c6d8
David Snider <david@davidsnider.org>
2022-01-30 18:37:03 -0700

    add sensor process

---
+++ src/cava/sensors/weather/tomorrow_io.py
@@ -0,0 +1,14 @@
+import cava
+from cava.models.tomorrow_io import weather_observation
+import time
+
+log = cava.log()
+
+while True:  # We're gonna run forever!!!!
+    log.info("Getting forecast data from tomorrow.io")
+    current_observation = weather_observation.create_from_live_data()
+    current_observation.log_observation()
+
+    # Pass the result to Cava and then sleep
+    current_observation.publish_to_cava()
+    time.sleep(60)
